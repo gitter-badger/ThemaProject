2821958af2f366d9558f34d316d4664bcc8eef80
martijn <m.balsmeer@gmail.com>
2015-02-11 21:25:06 +0100

    Database query wat uitgewerkt, INSERT werkt nog niet goed

--- src/database/Query.java
+++ src/database/Query.java
@@ -2,27 +2,93 @@ package database;
 
 import java.sql.*;
 
-public class Query
-{
-  public static void main( String args[] )
-  {
-    Connection c = null;
-    try {
-      Class.forName("org.sqlite.JDBC");
-      c = DriverManager.getConnection("jdbc:sqlite:test.db");
-    } catch ( Exception e ) {
-      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-      System.exit(0);
-    }
-    System.out.println("Opened database successfully");
-  }
-  
-  /**
-   * Test functie
-   * Gebruikt voor package testing
-   * @return {@link Object}
-   */
-  public Object test(){
-	  return "test";
-  }
+public class Query {
+
+	/**
+	 * getArtikelen() geeft een lijst met de artikelen die nu in de database
+	 * staan.
+	 * 
+	 * @return {@link String[]}
+	 * @throws SQLException
+	 */
+	public String[] getArtikelen(String database, String column) throws SQLException {
+		Statement stmt = makeConnection();
+		ResultSet rs = stmt.executeQuery("SELECT * FROM " + database + ";");
+		int i = 0;
+		String[] x = null;
+		while (rs.next()) {
+			x[i] = rs.getString(column);
+			i++;
+		}
+		return x;
+	}
+
+	/**
+	 * setArtikelen zet een nieuw artikel in de lijst met artikelen
+	 * 
+	 * @return {@link Boolean}
+	 * @throws SQLException
+	 */
+	public boolean setArtikel(String table, String naam) throws SQLException {
+		Statement stmt = makeConnection();
+		stmt.executeUpdate("INSERT INTO " + table + " (naam) VALUES (" + naam + ");");
+		return true;
+	}
+
+	/**
+	 * Voor het uitvoeren van rauwe SQL statements. Dit return een ResultSet en
+	 * hiervoor moet SQLlite geimporteerd worden (Eclipse zou dit zelf moeten
+	 * regelen)
+	 * 
+	 * @param input
+	 * @return {@link ResultSet}
+	 * @throws SQLException
+	 */
+	public ResultSet rawSQL(String input) throws SQLException {
+		Statement stmt = makeConnection();
+		return stmt.executeQuery(input);
+	}
+
+	/**
+	 * Maakt een table in de gewenste database
+	 * 
+	 * @param naam
+	 * @throws SQLException
+	 */
+	public void makeTable(String naam) throws SQLException {
+		Statement stmt = makeConnection();
+		stmt.executeUpdate("CREATE TABLE " + naam + " (naam TEXT NOT NULL);");
+	}
+
+	/**
+	 * Controlleer of database bestaat
+	 * 
+	 * @param naam
+	 * @return {@link boolean}
+	 */
+	public boolean doesDatabaseExists(String naam) {
+		// TODO: Maken
+		return true;
+	}
+
+	/**
+	 * Maakt database connection, niet bedoeld voor gebruik buiten de klasse
+	 * Query
+	 * 
+	 * @return {@link Statement}
+	 * @throws SQLException
+	 */
+	private Statement makeConnection() throws SQLException {
+		Connection c = null;
+		Statement stmt;
+		try {
+			Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection("jdbc:sqlite:test.db");
+		} catch (Exception e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
+		}
+		stmt = c.createStatement();
+		return stmt;
+	}
 }
--- src/frontend/Main.java
+++ src/frontend/Main.java
@@ -1,10 +1,20 @@
 package frontend;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import database.*;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		Query q = new Query();
-		System.out.println(q.test());
+		q.makeTable("test4");
+		q.setArtikel("test4", "blark");
+		q.setArtikel("test4", "blark2");
+		
+		String[] rs = q.getArtikelen("artikelen", "naam");
+		for (String m : rs){
+			System.out.println(m);
+		}
 	}
 }
